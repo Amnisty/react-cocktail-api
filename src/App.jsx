d955dd890aac096f94fbe0b531e92e3d133590c4
Amnisty <cduran140102@gmail.com>
2023-06-03 16:47:25 -0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+  const url = 'https://pokeapi.co/api/v2/pokemon/'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState({})
+  
+  async function searchPokemon() {
+  const chiringuito = await(await fetch(url)).json()
+  console.log(chiringuito)
+  setPokemon(chiringuito)
+}
 
+  useEffect(() => {
+    searchPokemon()
+  }, [])
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>{pokemon?.results?.map(cariñosas => <li>{cariñosas.name}</li>)}</div>    
   )
 }
 
